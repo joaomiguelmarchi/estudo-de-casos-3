772f5e13cbc4634bd3c1e14d3ddbef88d706cc87
João <jmsmarchi@gmail.com>
2022-11-29 20:54:26 -0300

    HIV

--- a.sql
+++ a.sql
@@ -3,85 +3,85 @@ DROP DATABASE hospital;
 -- Schema hospital
 -- -----------------------------------------------------
 
-CREATE DATABASE `hospital`;
-USE `hospital`;
+CREATE DATABASE hospital;
+USE hospital;
 
 -- -----------------------------------------------------
 -- Table `hospital`.`pacientes`
 -- -----------------------------------------------------
-CREATE TABLE `paciente` (
-  `cpf_paciente` INT NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `sexo` CHAR(1) NOT NULL,
-  PRIMARY KEY (`cpf_paciente`));
+CREATE TABLE paciente (
+  cpf_paciente INT NOT NULL,
+  nome_paciente VARCHAR(45) NOT NULL,
+  sexo CHAR(1) NOT NULL,
+  PRIMARY KEY (cpf_paciente));
 
 
 -- -----------------------------------------------------
 -- Table `hospital`.`medico`
 -- -----------------------------------------------------
-CREATE TABLE `medico` (
-  `crm` INT NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `salario` INT NOT NULL,
-  `data_nasc` DATE NOT NULL,
-  PRIMARY KEY (`crm`));
+CREATE TABLE medico (
+  crm INT NOT NULL,
+  nome VARCHAR(45) NOT NULL,
+  salario INT NOT NULL,
+  data_nasc DATE NOT NULL,
+  PRIMARY KEY (crm));
 
 
 -- -----------------------------------------------------
 -- Table `hospital`.`equipamentos`
 -- -----------------------------------------------------
-CREATE TABLE  `equipamentos` (
-  `id_equipamento` INT NOT NULL,
-  `nome_equipamento` VARCHAR(45) NOT NULL,
-  `quantidade` INT NOT NULL,
-  `funcao` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_equipamento`));
+CREATE TABLE  equipamentos (
+  id_equipamento INT NOT NULL,
+  nome_equipamento VARCHAR(45) NOT NULL,
+  quantidade INT NOT NULL,
+  funcao VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id_equipamento));
 
 
 -- -----------------------------------------------------
 -- Table `hospital`.`remedios`
 -- -----------------------------------------------------
-CREATE TABLE `remedios` (
-  `id_remedio` INT NOT NULL,
-  `nome_remedio` VARCHAR(45) NOT NULL,
-  `qtd` INT NOT NULL,
-  `funcao` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_remedio`));
+CREATE TABLE remedios (
+  id_remedio INT NOT NULL,
+  nome_remedio VARCHAR(45) NOT NULL,
+  qtd INT NOT NULL,
+  funcao VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id_remedio));
 
 -- -----------------------------------------------------
 -- Table `mydb`.`consulta`
 -- -----------------------------------------------------
-CREATE TABLE `consulta` (
-  `id_consulta` INT NOT NULL AUTO_INCREMENT,
-  `funcionario_cpf` INT NOT NULL,
-  `paciente_cpf_paciente` INT NOT NULL,
-  PRIMARY KEY (`id_consulta`, `funcionario_cpf`, `paciente_cpf_paciente`),
-    FOREIGN KEY (`funcionario_cpf`)REFERENCES medico (`crm`),
-    FOREIGN KEY (`paciente_cpf_paciente`)REFERENCES `paciente` (`cpf_paciente`)
+CREATE TABLE consulta (
+  id_consulta INT NOT NULL AUTO_INCREMENT,
+  funcionario_crm INT NOT NULL,
+  cpf_paciente INT NOT NULL,
+  PRIMARY KEY (id_consulta, funcionario_crm, cpf_paciente),
+    FOREIGN KEY (funcionario_crm)REFERENCES medico (crm),
+    FOREIGN KEY (cpf_paciente)REFERENCES paciente (cpf_paciente)
     );
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`equipamentos_has_consulta`
 -- -----------------------------------------------------
-CREATE TABLE `equipamentos_has_consulta` (
-  `equipamentos_id_equipamento` INT NOT NULL,
-  `consulta_id_consulta` INT NOT NULL,
-  PRIMARY KEY (`equipamentos_id_equipamento`, `consulta_id_consulta`),
-    FOREIGN KEY (`equipamentos_id_equipamento`)REFERENCES `equipamentos` (`id_equipamento`),
-    FOREIGN KEY (`consulta_id_consulta`)REFERENCES `consulta` (`id_consulta`)
+CREATE TABLE equipamentos_has_consulta (
+  equipamentos_id_equipamento INT NOT NULL,
+  consulta_id_consulta INT NOT NULL,
+  PRIMARY KEY (equipamentos_id_equipamento, consulta_id_consulta),
+    FOREIGN KEY (equipamentos_id_equipamento)REFERENCES equipamentos (id_equipamento),
+    FOREIGN KEY (consulta_id_consulta)REFERENCES consulta (id_consulta)
     );
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`remedios_has_consulta`
 -- -----------------------------------------------------
-CREATE TABLE `remedios_has_consulta` (
-  `remedios_id_remedio` INT NOT NULL,
-  `consulta_id_consulta` INT NOT NULL,
-  PRIMARY KEY (`remedios_id_remedio`, `consulta_id_consulta`),
-    FOREIGN KEY (`remedios_id_remedio`) REFERENCES `remedios` (`id_remedio`),
-    FOREIGN KEY (`consulta_id_consulta`) REFERENCES `consulta` (`id_consulta`));
+CREATE TABLE remedios_has_consulta (
+  remedios_id_remedio INT NOT NULL,
+  consulta_id_consulta INT NOT NULL,
+  PRIMARY KEY (remedios_id_remedio, consulta_id_consulta),
+    FOREIGN KEY (remedios_id_remedio) REFERENCES remedios (id_remedio),
+    FOREIGN KEY (consulta_id_consulta) REFERENCES consulta (id_consulta));
 
 
     /*INSERTS MEDICO*/
@@ -97,16 +97,16 @@ insert into medico (crm, nome, salario, data_nasc) values (989.074, 'Janene', 59
 insert into medico (crm, nome, salario, data_nasc) values (172.139, 'Bone', 6601.17, '2001-08-20');
 
     /*INSERTS PACIENTE*/
-insert into paciente (cpf_paciente, nome, sexo) values (474878789, 'Juieta', 'F');
-insert into paciente (cpf_paciente, nome, sexo) values (586198056, 'Lazar', 'M');
-insert into paciente (cpf_paciente, nome, sexo) values (303012054, 'Haze', 'M');
-insert into paciente (cpf_paciente, nome, sexo) values (967158696, 'Dalis', 'M');
-insert into paciente (cpf_paciente, nome, sexo) values (660662633, 'Paolo', 'M');
-insert into paciente (cpf_paciente, nome, sexo) values (656770798, 'Jedidiah', 'M');
-insert into paciente (cpf_paciente, nome, sexo) values (385943376, 'Emlyn', 'F');
-insert into paciente (cpf_paciente, nome, sexo) values (288996147, 'Hermine', 'F');
-insert into paciente (cpf_paciente, nome, sexo) values (213756192, 'Jaclyn', 'F');
-insert into paciente (cpf_paciente, nome, sexo) values (875193508, 'Franchot', 'M');
+insert into paciente (cpf_paciente, nome_paciente, sexo) values (474878789, 'Juieta', 'F');
+insert into paciente (cpf_paciente, nome_paciente, sexo) values (586198056, 'Lazar', 'M');
+insert into paciente (cpf_paciente, nome_paciente, sexo) values (303012054, 'Haze', 'M');
+insert into paciente (cpf_paciente, nome_paciente, sexo) values (967158696, 'Dalis', 'M');
+insert into paciente (cpf_paciente, nome_paciente, sexo) values (660662633, 'Paolo', 'M');
+insert into paciente (cpf_paciente, nome_paciente, sexo) values (656770798, 'Jedidiah', 'M');
+insert into paciente (cpf_paciente, nome_paciente, sexo) values (385943376, 'Emlyn', 'F');
+insert into paciente (cpf_paciente, nome_paciente, sexo) values (288996147, 'Hermine', 'F');
+insert into paciente (cpf_paciente, nome_paciente, sexo) values (213756192, 'Jaclyn', 'F');
+insert into paciente (cpf_paciente, nome_paciente, sexo) values (875193508, 'Franchot', 'M');
 
  /*INSERTS REMEDIOS */
 insert into remedios (id_remedio, nome_remedio, qtd, funcao) values (7506, 'Methoxsalen', 1, 'curar');
@@ -122,7 +122,7 @@ insert into remedios (id_remedio, nome_remedio, qtd, funcao) values (4862, 'Alum
 
 /*INSERTS EQUIPAMENTOS */
 insert into equipamentos (id_equipamento, nome_equipamento, quantidade, funcao) values (4317, 'bisturi', 0, 'cortar');
-insert into equipamentos (id_equipamento, nome_equipamento, quantidade, funcao) values (8854, 'laço', 8, 'amarrar');
+insert into equipamentos (id_equipamento, nome_equipamento, quantidade, funcao) values (8854, 'laco', 8, 'amarrar');
 insert into equipamentos (id_equipamento, nome_equipamento, quantidade, funcao) values (9025, 'fita', 3, 'colar');
 insert into equipamentos (id_equipamento, nome_equipamento, quantidade, funcao) values (3995, 'gaze', 11, 'tampar');
 insert into equipamentos (id_equipamento, nome_equipamento, quantidade, funcao) values (7669, 'maca', 5, 'deitar');
@@ -133,9 +133,76 @@ insert into equipamentos (id_equipamento, nome_equipamento, quantidade, funcao) 
 insert into equipamentos (id_equipamento, nome_equipamento, quantidade, funcao) values (9471, 'soro', 80, 'limpar');
 
 /*UPDATES MEDICO*/
+UPDATE medico SET crm = 123.456, nome = 'Joao', salario = 9000.00, data_nasc = '2005-02-17' WHERE crm = 912;
+UPDATE medico SET crm = 398.086, nome = 'Eliezer', salario = 8542.45, data_nasc = '2005-01-20' WHERE crm = 603;
+UPDATE medico SET crm = 008.890, nome = 'Sam', salario = 0020.50, data_nasc = '2004-05-6' WHERE crm = 172;
+UPDATE medico SET crm = 800.431, nome = 'Enzo', salario = 1111.01, data_nasc = '2005-04-23' WHERE crm = 989;
+UPDATE medico SET crm = 133.954, nome = 'Caua', salario = 6120.90, data_nasc = '2003-10-01' WHERE crm = 151;
 
 /*UPDATES PACIENTE*/
+UPDATE paciente SET cpf_paciente = 123456789, nome_paciente = 'Joao', sexo = 'M' WHERE cpf_paciente = 967158696 ;
+UPDATE paciente SET cpf_paciente = 987654321, nome_paciente = 'Bruna', sexo = 'F' WHERE cpf_paciente = 875193508 ;
+UPDATE paciente SET cpf_paciente = 549655730, nome_paciente = 'Eliezer', sexo = 'M' WHERE cpf_paciente = 474878789 ;
+UPDATE paciente SET cpf_paciente = 432998665, nome_paciente = 'Caua', sexo = 'M' WHERE cpf_paciente = 213756192;
+UPDATE paciente SET cpf_paciente = 877611203, nome_paciente = 'Sam', sexo = 'M' WHERE cpf_paciente = 660662633;
 
 /*UPDATES REMEDIOS */
+UPDATE remedios SET id_remedio = 1234, nome_remedio = 'Viagra', qtd = 69, funcao = 'Subir' WHERE id_remedio = 4862;
+UPDATE remedios SET id_remedio = 4321, nome_remedio = 'Codeina', qtd = 5, funcao = 'Chapar' WHERE id_remedio = 1310;
+UPDATE remedios SET id_remedio = 5544, nome_remedio = 'Acido', qtd = 80, funcao = 'Matar' WHERE id_remedio = 2369;
+UPDATE remedios SET id_remedio = 1987, nome_remedio = 'Dipirona', qtd = 1, funcao = 'Curar' WHERE id_remedio = 1177;
+UPDATE remedios SET id_remedio = 5091, nome_remedio = 'Laxante', qtd = 10, funcao = 'Soltar' WHERE id_remedio = 4862;
 
 /*UPDATES EQUIPAMENTOS */
+UPDATE equipamentos SET id_equipamento = 1234, nome_equipamento = 'Desfibrilador', quantidade = 7, funcao = 'Choque' WHERE id_equipamento = 9471;
+UPDATE equipamentos SET id_equipamento = 4321, nome_equipamento = 'Estetoscopio', quantidade = 39, funcao = 'Ouvir' WHERE id_equipamento = 6650;
+UPDATE equipamentos SET id_equipamento = 7653, nome_equipamento = 'Oximetro', quantidade = 53, funcao = 'Oxigenio' WHERE id_equipamento = 2095;
+UPDATE equipamentos SET id_equipamento = 9008, nome_equipamento = 'Hamper', quantidade = 100, funcao = 'Lixo' WHERE id_equipamento = 6344;
+UPDATE equipamentos SET id_equipamento = 5231, nome_equipamento = 'Eletrocardiografo', quantidade = 3, funcao = 'Eletrocardiograma' WHERE id_equipamento = 1702;
+
+/*DELETES MEDICO*/
+DELETE FROM medico WHERE crm = 178;
+DELETE FROM medico WHERE crm = 123;
+DELETE FROM medico WHERE crm = 717;
+DELETE FROM medico WHERE crm = 9;
+DELETE FROM medico WHERE crm = 887;
+
+/*DELETES PACIENTE*/
+DELETE FROM paciente WHERE cpf_paciente = 123456789;
+DELETE FROM paciente WHERE cpf_paciente = 987654321;
+DELETE FROM paciente WHERE cpf_paciente = 656770798;
+DELETE FROM paciente WHERE cpf_paciente = 303012054;
+DELETE FROM paciente WHERE cpf_paciente = 586198056;
+
+/*DELETES REMEDIOS */
+DELETE FROM remedios WHERE id_remedio = 7506;
+DELETE FROM remedios WHERE id_remedio = 4321;
+DELETE FROM remedios WHERE id_remedio = 1234;
+DELETE FROM remedios WHERE id_remedio = 9602;
+DELETE FROM remedios WHERE id_remedio = 9770;
+
+/*DELETES EQUIPAMENTOS */
+DELETE FROM equipamentos WHERE id_equipamento = 7653;
+DELETE FROM equipamentos WHERE id_equipamento = 4321;
+DELETE FROM equipamentos WHERE id_equipamento = 1234;
+DELETE FROM equipamentos WHERE id_equipamento = 7669;
+DELETE FROM equipamentos WHERE id_equipamento = 9025;
+
+/*SELECTS*/
+SELECT * FROM medico ORDER BY salario ASC;
+SELECT * FROM paciente ORDER BY cpf_paciente ASC;
+SELECT * FROM remedios ORDER BY qtd ASC;
+SELECT * FROM equipamentos ORDER BY quantidade ASC;
+
+SELECT nome FROM medico WHERE crm = 800;
+SELECT nome_paciente FROM paciente WHERE cpf_paciente = 385943376;
+SELECT nome_remedio FROM remedios WHERE id_remedio = 2672;
+SELECT nome_equipamento FROM equipamentos WHERE id_equipamento = 4317;
+
+
+/*
+1- INT n aceita mais de 9 caracteres.
+2- Where n aceita string
+3-Chaves Estrangeiras n funfam
+4-Inner Join
+*/
